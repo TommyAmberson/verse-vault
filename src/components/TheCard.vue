af8017238cd0e2d56aae6429fa19a31a05268c42
Tom Amberson <tom.amberson@ravenind.com>
2023-10-04 21:02:46 -0600

    WIP

---
+++ src/components/TheCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  book: string
+  chapter: string
+  verse: string
+  text: string
+}>()
+</script>
+
+<template>
+  <div class="card">
+
+    <h1>{{book}} {{chaper}}:{{verse}}</h1>
+    <p>{{text}}</p>
+
+  </div>
+</template>
---
+++ src/views/CardView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import gepc from '../data/gepc.json'
+import TheCard from '../components/TheCard.vue'
+
+</script>
+
+<template>
+  <main>
+    <TheCard verse='1' chapter='0' />
+  </main>
+</template>
