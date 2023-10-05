49ca3258af852c466d18428c2317ac8fd9ca8c22
Tom Amberson <tom.amberson@ravenind.com>
2023-10-04 23:16:27 -0600

    WIP frontend tutorial

--- laravel-backends-for-vuejs-3/pages/login.vue
+++ laravel-backends-for-vuejs-3/pages/login.vue
@@ -1,29 +1,46 @@
 <script setup lang="ts">
+import axios from "axios";
+
 definePageMeta({
-  layout: "centered",
+    layout: "centered",
 });
+
+interface LoginPayload {
+    email: string;
+    password: string;
+}
+
+const form = ref({
+    email: "",
+    password: "",
+});
+
+async function login(payload: LoginPayload) {
+    await axios.post("/login", payload);
+    useRouter().replace("/me")
+}
 </script>
 <template>
-  <div class="login">
-    <h1>Login</h1>
-    <form>
-      <label>
-        <div>Email</div>
-        <input type="text" />
-      </label>
+    <div class="login">
+        <h1>Login</h1>
+        <form @submit.prevent="login(form)">
+            <label>
+                <div>Email</div>
+                <input type="text" v-model="form.email" />
+            </label>
 
-      <label>
-        <div>Password</div>
-        <input type="password" />
-      </label>
-      <button class="btn">Login</button>
-    </form>
+            <label>
+                <div>Password</div>
+                <input type="password" v-model="form.password" />
+            </label>
+            <button class="btn">Login</button>
+        </form>
 
-    <p>
-      Don't have an account?
-      <NuxtLink class="underline text-lime-600" to="/register"
-        >Register now!</NuxtLink
-      >
-    </p>
-  </div>
+        <p>
+            Don't have an account?
+            <NuxtLink class="underline text-lime-600" to="/register"
+                >Register now!</NuxtLink
+            >
+        </p>
+    </div>
 </template>
